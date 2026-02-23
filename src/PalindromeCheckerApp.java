8b4cde84dde45a718b66b426add18d2456d50057
shivanimahesh10 <shivaanimahesh10@gmail.com>
2026-02-23 09:56:35 +0530

    UC1 and UC2 completed

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,13 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
         System.out.println("Welcome to palindrome checker app");
+        String word = "madam";
+        String reversed = new StringBuilder(word).reverse().toString();
+        if(word.equalsIgnoreCase(reversed)) {
+            System.out.println(word + "is a palindrome");
+        }else{
+            System.out.println(word+"is not a palindrome");
+        }
     }
 }
+
